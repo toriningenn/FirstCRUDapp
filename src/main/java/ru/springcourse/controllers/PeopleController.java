4fa7b4e6cfb7f80635671d2b793a67580063d65b
toriningenn <annamihailova72@yandex.com>
2020-12-23 15:03:14 +0100

    CRUD practice

--- src/main/java/ru/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/springcourse/controllers/PeopleController.java
@@ -12,26 +12,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.sql.SQLException;
 
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
-//это чтобы мы могли обращаться к personDAO в этом методе
-private final PersonDAO personDAO;
-/*можно писать @Autowired а можно не писать, спринг всё равно сам внедрит зависимость. контроллер всегда будет
-    создаваться с этим дао*/
+    //это чтобы мы могли обращаться к personDAO в этом методе
+    private final PersonDAO personDAO;
+
+    /*можно писать @Autowired а можно не писать, спринг всё равно сам внедрит зависимость. контроллер всегда будет
+        создаваться с этим дао*/
     public PeopleController(PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
     @GetMapping()
     public String index(Model model) {
-    //получим всех людей из DAO и передадим на отображение в представление
-        model.addAttribute("people",personDAO.index());
-    return "people/index";
-}
-//в URL можно будет вставить любой айди, он будет извлечен и вставлен в параметры метода
+        //получим всех людей из DAO и передадим на отображение в представление
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    //в URL можно будет вставить любой айди, он будет извлечен и вставлен в параметры метода
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         //получим одного человека по id из DAO и передадим на отображение в представление
@@ -46,33 +49,32 @@ private final PersonDAO personDAO;
     }
 
     @PostMapping()
-public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) throws SQLException {
         //создастся из данных формы
-     if (bindingResult.hasErrors()) {
-         return "people/new";
-     }
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
-    
+
     @GetMapping("/{id}/edit")
-    public String edit(Model model,@PathVariable("id") int id){
+    public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("person", personDAO.show(id));
         //в самой форме редактирования будут значения этого человека
         return "people/edit";
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id)
-    { if (bindingResult.hasErrors()) {
-        return "people/edit";
-    }
-
-        personDAO.update(id,person);
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+personDAO.update(id,person);
         return "redirect:/people";}
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
+ @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) throws SQLException {
         personDAO.delete(id);
         return "redirect:/people"; }
 }
--- src/main/java/ru/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/springcourse/dao/PersonDAO.java
@@ -3,43 +3,111 @@ package ru.springcourse.dao;
 import org.springframework.stereotype.Component;
 import ru.springcourse.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
+//убрать в properties
+private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+private static final String USERNAME = "postgres";
+private static final String PASSWORD = "postgres";
+private static Connection connection;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Bob","bobkek@yandex.com",32));
-        people.add(new Person(++PEOPLE_COUNT, "Tom","tomkek@yandex.com",32));
-        people.add(new Person(++PEOPLE_COUNT, "Lol","lolkek@yandex.com",21));
-        people.add(new Person(++PEOPLE_COUNT, "John","johnkek@yandex.com",54));
+static {
+    try {
+        Class.forName("org.postgresql.Driver");
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
     }
 
+    try {
+        connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    } catch (SQLException throwables) {
+        throwables.printStackTrace();
+    }
+}
+
+
     public List<Person> index() {
+    //добавляет людей по умолчанию
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultset = statement.executeQuery(SQL);
+
+            while(resultset.next()) {
+            Person person = new Person();
+            person.setEmail(resultset.getString("email"));
+            person.setAge(resultset.getInt("age"));
+            person.setId(resultset.getInt("id"));
+            person.setName(resultset.getString("name"));
+
+            people.add(person);
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
 
-    public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+public Person show(int id) {
+    Person person = null;
+    // в случае если случится SQLException, то метод выплюнет null.
+    //если создавать объект в блоке try, он не будет виден ретерну.
+    try {
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from Person where id=?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        //сдвигаем указатель один раз и он будет показывать первую строчку из БД
+        person = new Person();
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+    } catch (SQLException throwables) {
+        throwables.printStackTrace();
+    }
+    return person;
     }
 
-    public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+  public void save(Person person) throws SQLException {
+      //пока что по умолчанию всегда id=1
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "INSERT INTO Person VALUES(1,?,?,?)");
+
+        preparedStatement.setString(1,person.getName());
+        preparedStatement.setInt(2,person.getAge());
+        preparedStatement.setString(3,person.getEmail());
+        preparedStatement.executeUpdate();
     }
 
     public void update(int id, Person person) {
-        Person personToBeUpdated = show(id);
-personToBeUpdated.setName(person.getName());
-personToBeUpdated.setAge(person.getAge());
-personToBeUpdated.setEmail(person.getEmail());
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "UPDATE Person SET name=?,age=?,email=? WHERE id=?");
+//где id=? мы обновляем колонки
+            preparedStatement.setInt(4,id);
+            preparedStatement.setInt(2,person.getAge());
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setString(3, person.getEmail());
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public void delete(int id) {
-        people.removeIf(p -> p.getId()==id);
+public void delete(int id) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "DELETE from Person WHERE id=?");
+        preparedStatement.setInt(1,id);
+        preparedStatement.executeUpdate();
     }
 }
